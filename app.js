388665b7106431e1f83f30a53afd1fd84d673a4f
Brian Papabear Gerhards <briangerhards@gmail.com>
2024-10-01 21:03:50 -0500

    adds sequelize and logic to main application

---
+++ app.js
@@ -0,0 +1,11 @@
+const interval = 5 * 60 ;
+
+const backgroundTask = () => {
+    console.log('Hello world');
+
+    // connect to database
+    // retrieve all records from the database
+    // ping each one to see if they exist
+}
+
+setInterval(backgroundTask, interval);
